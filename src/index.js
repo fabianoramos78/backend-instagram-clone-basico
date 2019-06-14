ac07cb60669dc321095b9ae245178efc5636d4c4
Fabiano Ramos <contato@fabianoramos.dev>
2019-06-14 20:23:36 -0300

    Implementação ESLint

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://USERNAME:PASSWORD@cluster0-qsofr.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-}); //PASSWORD E USERNAME OBTIDOS NO MONGODB ATLAS (https://cloud.mongodb.com)
+mongoose.connect('mongodb+srv://fabianoramos:mix141501@cluster0-qsofr.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+}); // PASSWORD E USERNAME OBTIDOS NO MONGODB ATLAS (https://cloud.mongodb.com)
 
 app.use((req, res, next) => {
-    req.io = io;
-    next();
-}) ;
+  req.io = io;
+  next();
+});
 
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..','uploads','resized')));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
